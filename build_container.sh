fdd5eaf4402d6f7f4c003fd32d93970620eb1be0
otakuto <otakuto893@gmail.com>
2017-11-29 02:39:44 +0900

    add ccache and equery

--- build_container.sh
+++ build_container.sh
@@ -1,1 +1,5 @@
 docker build docker -t gentoo:gbs
+cid=$(docker run --rm -v /usr/portage/distfiles:/usr/portage/distfiles -v /var/db/repos/gentoo:/var/db/repos/gentoo:ro -d -i -t gentoo:gbs)
+docker exec ${cid} emerge ccache
+docker exec ${cid} emerge gentoolkit
+docker commit ${cid} gentoo:gbs
