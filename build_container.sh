e7ef90e66fc3a7f0fbbf9d2819955ec2abeb96c5
otakuto <otakuto893@gmail.com>
2017-12-12 04:45:53 +0900

    update gbs image

--- build_container.sh
+++ build_container.sh
@@ -1,5 +1,10 @@
+GBS_DIR=/home/user/work/git/gentoo-build-server/gbs
+mkdir -p ${GBS_DIR}/repos
+mkdir -p ${GBS_DIR}/distfiles
+
+docker pull gentoo/stage3-amd64:latest
 docker build docker -t gentoo:gbs
-cid=$(docker run --rm -v /usr/portage/distfiles:/usr/portage/distfiles -v /var/db/repos/gentoo:/var/db/repos/gentoo:ro -d -i -t gentoo:gbs)
+cid=$(docker run --rm -v ${GBS_DIR}/distfiles:/usr/portage/distfiles -v ${GBS_DIR}:/var/db/repos/gentoo:ro -d -i -t gentoo:gbs)
 docker exec ${cid} emerge ccache
 docker exec ${cid} emerge gentoolkit
 docker commit ${cid} gentoo:gbs
