6054069ffaed3e839f12396a5fd1d0b5951172fa
otakuto <otakuto893@gmail.com>
2017-12-12 08:52:12 +0900

    add curl

--- build_container.sh
+++ build_container.sh
@@ -8,6 +8,7 @@ mkdir -p ${GBS_DIR}/repos
 docker pull gentoo/stage3-amd64:latest
 docker build docker -t gentoo:gbs
 cid=$(docker run --rm -v ${GBS_DIR}/distfiles:/usr/portage/distfiles -v ${GBS_DIR}/repos:/var/db/repos:ro -d -i -t gentoo:gbs)
-docker exec ${cid} emerge ccache
-docker exec ${cid} emerge gentoolkit
+docker exec ${cid} emerge dev-util/ccache
+docker exec ${cid} emerge app-portage/gentoolkit
+docker exec ${cid} emerge net-misc/curl
 docker commit ${cid} gentoo:gbs
