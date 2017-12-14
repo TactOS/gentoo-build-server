393ba87a561dca255cb5dddf4de4189851ccb71d
otakuto <otakuto893@gmail.com>
2017-12-14 15:21:30 +0900

    cosme

--- build_container.sh
+++ build_container.sh
@@ -7,7 +7,7 @@ mkdir -p ${GBS_DIR}/repos
 
 docker pull gentoo/stage3-amd64:latest
 docker build docker -t gentoo:gbs
-cid=$(docker run --rm -v ${GBS_DIR}/distfiles:/usr/portage/distfiles -v ${GBS_DIR}/repos:/var/db/repos:ro -d -i -t gentoo:gbs)
+cid=$(docker run --rm -v ${GBS_DIR}/distfiles:/usr/portage/distfiles -v ${GBS_DIR}/repos:/var/db/repos:ro -idt gentoo:gbs)
 docker exec ${cid} emerge dev-util/ccache
 docker exec ${cid} emerge app-portage/gentoolkit
 docker exec ${cid} emerge net-misc/curl
